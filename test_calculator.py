7447098cc5dc145be95080e8195d7c2796697166
kaliweiser <kaliweiser@gmail.com>
2026-06-18 09:44:20 -0400

    edited test_calculator file

--- test_calculator.py
+++ test_calculator.py
@@ -2,21 +2,22 @@ import unittest
 from calculator import Calculator
 
 class TestCalculator(unittest.TestCase):
+
     def test_addition(self):
-        self.assertEqual(addition(5, 3), 8)
+        self.assertEqual(Calculator.addition(3, 5), 8)
 
     def test_subtraction(self):
-        self.assertEqual(subtraction(7, 5), 2)
+        self.assertEqual(Calculator.subtraction(7, 5), 2)
 
     def test_multiplication(self):
-        self.assertEqual(multiplication(6, 7), 42)
+        self.assertEqual(Calculator.multiplication(6, 7), 42)
 
     def test_division(self):
-        self.assertEqual(division(4,2), 2)
+        self.assertEqual(Calculator.division(4, 2), 2)
 
     def test_zero_division(self):
         with self.assertRaises(ValueError):
-            division(9, 0)
+            Calculator.division(9, 0)
 
 if __name__ == '__main__':
     unittest.main()
