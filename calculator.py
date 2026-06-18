3d3361e6504713c7c2960bc7c315c83c02bcfc3a
kaliweiser <kaliweiser@gmail.com>
2026-06-18 09:43:06 -0400

    Adjusted Calculator class

--- calculator.py
+++ calculator.py
@@ -1,10 +1,16 @@
 class Calculator:
+    def __init__(self):
+        pass
+    @staticmethod
     def addition(x, y):
         return x + y
+    @staticmethod
     def subtraction(x, y):
-        return x/y
-    def multiplicatoin(x, y):
+        return x-y
+    @staticmethod
+    def multiplication(x, y):
         return x*y
+    @staticmethod
     def division(x, y):
         if y == 0:
             raise ValueError("Cannot divide by zero")
